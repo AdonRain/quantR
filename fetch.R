2ea993eebdbdbe34436b16a643a0426f599805f5
AdonRain <leids@foxmail.com>
2016-10-23 19:25:14 +0800

    save

--- fetch.R
+++ fetch.R
@@ -15,9 +15,11 @@ if(exists('date.end')){
 date.end <- Sys.Date();
 
 if(date.start < date.end){
-  date.period <- seq(date.start, date.end, 'day');
-  http.url <- 'http://www.csindex.com.cn/sseportal/csiportal/syl/hytype.do?code=3&zb_flg=2&db_type=0';
-  http.header <- c(
+  pel <- lapply(getURL(lapply(seq(date.start, date.end, 'day'), function(day) {
+    return(paste('http://www.csindex.com.cn/sseportal/csiportal/syl/hytype.do?code=3&zb_flg=2&db_type=0',
+      paste('date', day, sep = '='),
+      sep = '&'));
+  }), httpheader = c(
     'Accept' = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding' = 'gzip, deflate, sdch',
     'Accept-Language' = 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4',
@@ -28,15 +30,7 @@ if(date.start < date.end){
     'Pragma' = 'no-cache',
     'Upgrade-Insecure-Requests' = '1',
     'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
-  );
-
-  urls <- lapply(date.period, function(day) {
-    return(paste(http.url, paste('date', day, sep = '='), sep = '&'));
-  });
-
-  htmls <- getURL(urls, httpheader = http.header, .encoding="gbk");
-
-  pel <- lapply(htmls, function(html) {
+  ), .encoding="gbk"), function(html) {
     tmpMode <- regexpr(';沪深A股</div></td>\\r\\n\\t\\t\\t\\t <td width=\"10%\"><div align=\"right\">\\d+\\.\\d+&nbsp;&nbsp;</div>', html);
     if(tmpMode[1] != -1){
       tmpStr <- regmatches(html, tmpMode);
@@ -53,8 +47,12 @@ if(date.start < date.end){
   }
 }
 
-print(paste('最新日期', date.end, sep='：'));
-
 npev <- as.numeric(pev);
 
 plot(npev, type='l');
+abline(h=max(npev), col='red');
+abline(h=min(npev), col='blue');
+abline(h=mean(npev), col='green');
+abline(h=median(npev), col='gold');
+
+print(paste('最新日期', date.end, sep='：'));
